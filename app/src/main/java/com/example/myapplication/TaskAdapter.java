a4213e7f1ed6e26f3282c2b00c6f7f50f25e52fe
rula <rulaafanh2002@gmail.com>
2023-12-24 12:25:57 +0200

    version1

--- app/src/main/java/com/example/myapplication/TaskAdapter.java
+++ app/src/main/java/com/example/myapplication/TaskAdapter.java
@@ -6,29 +6,32 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import com.example.myapplication.R;
+
+import androidx.annotation.NonNull;
+
 import java.util.List;
 
 public class TaskAdapter extends ArrayAdapter<Task> {
 
-    private Context context;
-    private List<Task> tasks;
+    private final Context context;
+    private final List<Task> tasks;
 
     public TaskAdapter(Context context, List<Task> tasks) {
-        super(context, R.layout.task_list_item, tasks);
+        super(context, R.layout.activity_task_list, tasks);
         this.context = context;
         this.tasks = tasks;
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.task_list_item, parent, false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.activity_task_list, parent, false);
         }
 
         TextView titleTextView = convertView.findViewById(R.id.titleTextView);
         TextView dueDateTextView = convertView.findViewById(R.id.dueDateTextView);
-        CheckBox statusCheckBox = convertView.findViewById(R.id.statusCheckBox);
+        CheckBox statusCheckBox = convertView.findViewById(R.id.statusTextView);
 
         Task task = tasks.get(position);
 
